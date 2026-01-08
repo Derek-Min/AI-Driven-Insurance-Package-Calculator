b8b70fcdff435353eace906fa447b7522f819ba1
Min Thant Wai <minthantwai.mr@gmail.com>
2026-01-08 21:55:35 +0800

    Update chatbot.js

--- trust-chatbot/functions/chatbot.js
+++ trust-chatbot/functions/chatbot.js
@@ -1,15 +1,19 @@
 export async function onRequestPost({ request }) {
     const body = await request.text();
 
-    const res = await fetch("http://127.0.0.1:8080/chatbot", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body
-    });
+    const res = await fetch(
+        "https://api.trust-insurancexyz.xyz/api/chatbot",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body
+        }
+    );
 
     return new Response(await res.text(), {
+        status: res.status,
         headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*"
